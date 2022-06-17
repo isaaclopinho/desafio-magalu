9b2dc8102dd104ac6c7f5a98fae2d3b266e917e2
Isaac Lopes <isaaclopinho@gmail.com>
2022-06-17 14:05:25 -0300

    typography test

--- src/components/atoms/Typography/Typography.spec.tsx
+++ src/components/atoms/Typography/Typography.spec.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { Typography } from './index';
+import { render, screen } from '@testing-library/react';
+import { Typography } from './Typography';
 
-it('renders correctly', () => {
-  // const {} = render(<Typography />);
+describe('Testing Typography Component', () => {
+  it('Heading typographies must have a heading text component', () => {
+    render(<Typography type="h1" weight="bold" color="primary" />);
+
+    const h1 = screen.getByRole('heading', { level: 1 });
+
+    expect(h1).toBeInTheDocument();
+  });
+
+  it('Paragraph typographies must have a paragraph component', () => {
+    render(
+      <Typography type="p1" weight="bold" color="primary">
+        marvel
+      </Typography>
+    );
+
+    const p = screen.getByText('marvel');
+
+    expect(p).toBeInTheDocument();
+    expect(p.tagName).toBe('P');
+  });
 });
